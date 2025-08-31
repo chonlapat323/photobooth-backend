1777d0c7cca08859a6974222f55f9ccf58ac8939
chonlapat323 <cschonlapat@gmail.com>
2025-08-31 20:07:32 +0700

    Add 4 modules: auth, users, photos, albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PhotosModule } from './photos/photos.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PhotosModule, AlbumsModule],
   controllers: [AppController],
   providers: [AppService],
 })
